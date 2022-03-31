c3ced5693ebb1dce9942e0c8c7f3eff7ecb43cc2
Radish <53844660+radixsh@users.noreply.github.com>
2022-03-30 22:42:53 -0700

    Begin setup shell script

---
+++ setup.sh
@@ -0,0 +1,10 @@
+
+# get brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# use brew
+brew install node
+brew install neovim
+
+# iceberg
+curl -o ~/.vim/colors/iceberg.vim --create-dirs https://raw.githubusercontent.com/cocopon/iceberg.vim/master/colors/iceberg.vim
